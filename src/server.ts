3dd23fe910e58c903809fd071b79b28c4cbd1d89
Luis Jacinto <luishjacinto98@gmail.com>
2020-08-06 23:46:30 -0300

    port

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import routes from './routes'
 import cors from 'cors'
 
+const port = process.env.PORT || 5000
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(5000)
+app.listen(port)
