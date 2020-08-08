162ae091743caadad01a1aff9c9625d1fb4c83db
Luis Jacinto <luishjacinto98@gmail.com>
2020-08-08 06:33:54 -0300

    commitando

--- src/database/seeds/02_generate_class_schedule.ts
+++ src/database/seeds/02_generate_class_schedule.ts
@@ -6,152 +6,152 @@ exports.seed = function(knex: knex) {
         return knex('class_schedule').insert([
             {
                 week_day: 1,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 1
             },
             {
                 week_day: 2,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 1
             },
             {
                 week_day: 3,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 1
             },
             {
                 week_day: 4,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 1
             },
             {
                 week_day: 5,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 1
             },
             {
                 week_day: 1,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 2
             },
             {
                 week_day: 2,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 2
             },
             {
                 week_day: 3,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 2
             },
             {
                 week_day: 4,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 2
             },
             {
                 week_day: 5,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 2
             },
             {
                 week_day: 1,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 2
             },
             {
                 week_day: 2,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 2
             },
             {
                 week_day: 3,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 2
             },
             {
                 week_day: 4,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 2
             },
             {
                 week_day: 5,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 2
             },
             {
                 week_day: 1,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 3
             },
             {
                 week_day: 2,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 3
             },
             {
                 week_day: 3,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 3
             },
             {
                 week_day: 4,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 3
             },
             {
                 week_day: 5,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 3
             },
             {
                 week_day: 1,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 4
             },
             {
                 week_day: 2,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 4
             },
             {
                 week_day: 3,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 4
             },
             {
                 week_day: 4,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 4
             },
             {
                 week_day: 5,
-                from: '08:00',
-                to: '12:00',
+                from: 480,
+                to: 1080,
                 class_id: 4
             }
         ]);
