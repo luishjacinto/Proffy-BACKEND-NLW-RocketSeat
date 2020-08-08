94e52f49db048fee82a5bc9bcfacb8fb5b3c49b7
Luis Jacinto <luishjacinto98@gmail.com>
2020-08-08 07:41:21 -0300

    class change

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -44,7 +44,7 @@ export default class ClassesController {
                         .whereRaw('class_schedule.from <= ??',[timeInMinutes])
                         .whereRaw('class_schedule.to >= ??',[timeInMinutes])
                 })
-                .where('classes.subject','like', `%${subject}%`)
+                .where('classes.subject','=', subject)
                 .join('users','classes.user_id','=','users.id')
                 .select(['classes.*','users.*'])   
 
